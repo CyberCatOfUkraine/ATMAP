1df16a6a75a599f6b990709583ec38b30b914cd7
KOT98 <tiger1998sasha@gmail.com>
2021-10-24 18:27:14 +0300

    LR 4 (upd)

--- app/src/main/java/com/shaman/labka/MainActivity.java
+++ app/src/main/java/com/shaman/labka/MainActivity.java
@@ -174,6 +174,7 @@ public class MainActivity extends AppCompatActivity {
                 if (_timeLeft<0)
                     return;
                 if (_timeLeft==0) {
+                    _correctAnswerTextView.setText(String.format("%s %s %s %s", getString(R.string.correct_answer_number), _rightAnswerNumber," з ",_countOfAttempt));
                     ShowToast("Час вийшов, правильних відповідей: " + _rightAnswerNumber+" з "+_countOfAttempt);
                     findViewById(R.id.btn_start_stop_reset).performClick();
                 }
